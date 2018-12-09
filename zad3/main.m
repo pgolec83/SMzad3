465a4ccf03e612c8f741c1de3fa8b1efd13c4a15
Admin <admin@Admins-Mac.local>
2018-12-09 22:52:40 +0300

    Initial Commit

---
+++ zad3/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  zad3
+//
+//  Created by Admin on 09.12.2018.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
